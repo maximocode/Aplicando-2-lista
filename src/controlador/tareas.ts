84d04834382b8474a6dfedc550cb6e632cd2852d
Maximo <maximomendana8@gmail.com>
2026-09-25 21:37:03 -0300

    Agrego funciones ver detalles y detalles

--- src/controlador/tareas.ts
+++ src/controlador/tareas.ts
@@ -1,7 +1,7 @@
 const prompt = require('prompt-sync')();
-const { Tareas} = require("./modelo/tareas.js");
+const { Tareas, buscarID} = require("./modelo/tareas.js");
 import type { Tarea, Estado } from "../modelo/tareas";
-const { menuPrincipal, menuVerTareas, verDetalles } = require("./vista/tareas.js");
+const { menuPrincipal, menuVerTareas, preguntarDetalle, mostrarDetalles} = require("./vista/tareas.js");
 
 function iniciarApp(): void {
     let op: number;
@@ -74,6 +74,37 @@ function mostrarPorEstado(Tareas: Tarea[], estado: Estado): number[] {
         }
     }
     return idsMostrados;
+}
+
+function verDetalles(Tareas: Tarea[], idsMostrados: number[]): void {
+    let aux: number;
+    do {
+        preguntarDetalle();
+        aux = parseInt(prompt());
+    } while (aux !== 0 && aux > idsMostrados.length);
+    if (aux !== 0) {
+        let tareaEncontrada = buscarID(Tareas, aux, idsMostrados);   
+        if (tareaEncontrada !== undefined) {
+        Detalles(tareaEncontrada);
+    } else {
+        console.log("No se encontró la tarea.");
+    }
+    } else {
+        console.log("Saliendo...");
+    }
+}
+
+function Detalles(tareaEncontrada: Tarea): void {
+    mostrarDetalles(tareaEncontrada);
+    let aux2;
+    do{
+        console.log("Desea editar algo de la tarea?\n");
+        console.log("[1] si" + "[0] no\n");
+        aux2 = parseInt(prompt());
+    }while (aux2 !== 0 && aux2 !== 1);
+    if(aux2 === 1){
+        editarTarea(tareaEncontrada);
+    }
 }
 
 
